dae1f3b94866e4df276e4cc272bacf841ba20d83
JamesMatthewsMohawk <James_Matthews@Mohawkind.com>
2025-06-10 05:29:22 -0500

    Add project files.

---
+++ DuneQOL/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim steamPath As String
+        If System.Environment.Is64BitOperatingSystem = True Then
+            steamPath = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam", "InstallPath", Nothing)
+        Else
+            steamPath = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", Nothing)
+        End If
+        MsgBox("Steam Folder is " & steamPath)
+    End Sub
+End Class
